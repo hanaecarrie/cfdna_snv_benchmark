4fa7cf5d0448ba001f1197e32c24ff4f52e46ede
hanaecarrie <hanae.carrie@free.fr>
2021-03-08 15:18:24 +0800

    [update]: use python script

--- analysis/supporting_reads.py
+++ analysis/supporting_reads.py
@@ -1,9 +1,10 @@
 import re
 import pysam
-from tqdm import tqdm
+import pandas as pd
+from tqdm.notebook import tqdm
 
 
-def list_supporting_reads(bamfile_path, vcf_df):
+def list_supporting_reads(bamfile_path, vcf_df, verbose=0):
     samfile = pysam.AlignmentFile(bamfile_path, "rb")
 
     # initiate list of reads to remove
@@ -21,7 +22,7 @@ def list_supporting_reads(bamfile_path, vcf_df):
         n = 0  # number of reads supporting the reference genome
         a = 0  # number of reads with alternative nucleotide (not ref, not alt snp)
         mutation_type = None
-        alt = []
+        # alt = []
 
         # iterate over reads that fall into the mutation position
         for read in samfile.fetch(str(mutation['#CHROM']), mutation['POS']-1, mutation['POS']):
@@ -29,7 +30,7 @@ def list_supporting_reads(bamfile_path, vcf_df):
             seq = read.query_alignment_sequence
             pos = (mutation['POS']-1) - read.reference_start
             cigar = read.cigarstring
-            cond, cond1, cond2 = False, False, False
+            cond = False
 
             # guess mutation type
             if ',' in mutation['ALT']:  # several possible variants
@@ -53,8 +54,7 @@ def list_supporting_reads(bamfile_path, vcf_df):
 
             ######## SNV ##########
             if mutation_type == 'SNV':
-                # cond1 = True as cigar string is not informative of SNVs
-                cond1 = True
+                # cond = sequence matching
                 if cigar is not None:
                     cigar_pos = re.split('M|I|D|N|S|H|P|=|X', cigar)[:-1]
                     cigar_states = re.split('[0-9]+', cigar)[1:]
@@ -70,79 +70,49 @@ def list_supporting_reads(bamfile_path, vcf_df):
                 if ',' in mutation['ALT']:
                     for muts in mutation['ALT'].split(','):
                         if seq[pos] == muts:
-                            cond2 = True
+                            cond = True
                 else:
                     if seq[pos] == mutation['ALT']:
-                        cond2 = True
+                        cond = True
 
-            ######## INSERTION ##########
-            elif mutation_type == 'INS':  # insertion
-                # cond1 = cigar string indicates a deletion at this position
+            ######## INSERTION / DELETION ##########
+            elif (mutation_type == 'INS') or (mutation_type == 'DEL'):  # indel
+                # cond = cigar string indicates a deletion at this position
                 if cigar is not None:
                     cigar_pos = re.split('M|I|D|N|S|H|P|=|X',cigar)[:-1]
                     cigar_states = re.split('[0-9]+', cigar)[1:]
-                    if 'I' in cigar_states:
+                    if mutation_type[0] in cigar_states:
                         cigar_pos = [0 if (cigar_states[i] == 'S') else int(ci) for i, ci in enumerate(cigar_pos)]
                         cigar_pos = [-int(ci) if (cigar_states[i] == 'D') else int(ci) for i, ci in enumerate(cigar_pos)]
-                        indexI = cigar_states.index('I')
-                        if type(indexI) == list:
-                            for idxI in indexI:
-                                print(cigar_pos[:idxI], pos)
-                                if sum(cigar_pos[:idxI]) == pos + 1:
-                                    cond1 = True
+                        indexINDEL = cigar_states.index(mutation_type[0])
+                        if type(indexINDEL) == list:
+                            for idxINDEL in indexINDEL:
+                                print(cigar_pos[:idxINDEL], pos)
+                                if sum(cigar_pos[:idxINDEL]) == pos + 1:
+                                    cond = True
                         else:
-                            if sum(cigar_pos[:indexI]) == pos + 1:
-                                cond1 = True
-                if cond1:
-                    # cond2 = nucleotide sequence comparison
-                    if ',' in mutation['ALT']:
-                        for muts in mutation['ALT'].split(','):
-                            if seq[pos:pos + len(muts)] == muts:
-                                cond2 = True
-                    else:
-                        cond2 = (seq[pos:pos+len(mutation['ALT'])] == mutation['ALT'])
-
-            ######## DELETION ##########
-            elif mutation_type == 'DEL':  # deletion
-                # cond1 = cigar string indicates a deletion at this position
-                if cigar is not None:
-                    cigar_pos = re.split('M|I|D|N|S|H|P|=|X',cigar)[:-1]
-                    cigar_states = re.split('[0-9]+',cigar)[1:]
-                    if 'D' in cigar_states:
-                        cigar_pos = [0 if (cigar_states[i] == 'S') else int(ci) for i, ci in enumerate(cigar_pos)]
-                        cigar_pos = [-int(ci) if (cigar_states[i] == 'D') else int(ci) for i, ci in enumerate(cigar_pos)]
-                        indexD = cigar_states.index('D')
-                        if type(indexD) == list:
-                            for idxD in indexD:
-                                if sum(cigar_pos[:idxD]) == pos + 1:
-                                    cond1 = True
-                        else:
-                            if sum(cigar_pos[:indexD]) == pos + 1:
-                                cond1 = True
-                if cond1:
-                    # cond2 = nucleotide sequence comparison
-                    if ',' in mutation['ALT']:
-                        if seq[pos:pos + len(muts)] == muts:
-                            cond2 = True
-                    else:
-                        cond2 = (seq[pos:pos+len(mutation['ALT'])] == mutation['ALT'])
-
-            if cond1 and cond2:
+                            if sum(cigar_pos[:indexINDEL]) == pos + 1:
+                                cond = True
+            if cond:
                 c += 1
                 reads2remove.append(read.query_name)
-            elif not cond1 and (seq[pos:pos+len(mutation['REF'])] == mutation['REF']):
+            elif not cond and (seq[pos:pos+len(mutation['REF'])] == mutation['REF']):
                 n += 1
             elif cigar is not None:
                 a += 1
-                alt.append(cond1)
-                alt.append(seq[pos:pos+max(len(mutation['ALT']), len(mutation['REF']))])
+                # alt.append(cond)
+                # alt.append(pos)
+                # alt.append(cigar)
+                # alt.append(seq[pos:pos+max(len(mutation['ALT']), len(mutation['REF']))])
             else:
                 p += 1
-
-        if mutation_type == 'SNV':
-            print('SNV', mutation['POS'], 'REF:', mutation['REF'], 'ALT:', mutation['ALT'], genotype, '% VAF:', round(100*c/t, 2))
-        else:
-            print(mutation_type,  mutation['POS'], 'REF:', mutation['REF'], 'ALT:', mutation['ALT'], c, n, t, p, '% VAF:', round(100*c/t, 2), alt)
+        if verbose == 0:
+            print(mutation_type, c, round(100*c/t, 2))
+        if verbose > 1:
+            if mutation_type == 'SNV':
+                print('SNV', mutation['POS'], 'REF:', mutation['REF'], 'ALT:', mutation['ALT'], genotype, '% VAF:', round(100*c/t, 2))
+            else:
+                print(mutation_type,  mutation['POS'], 'REF:', mutation['REF'], 'ALT:', mutation['ALT'], c, n, t, p, '% VAF:', round(100*c/t, 2))  # , alt)
         log_dict["position"].append(mutation['POS'])
         log_dict["type"].append(mutation_type)
         log_dict["total_reads"].append(t)
@@ -151,20 +121,6 @@ def list_supporting_reads(bamfile_path, vcf_df):
         log_dict["alternative_reads"].append(a)
         log_dict["problematic_reads"].append(p)
 
-    samfile.close()
-
-    return reads2remove, log_dict
-
-
-if __name__ == '__main__':
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-
-    bamfile_path = '../data/healthy_chr22_merged-ready.bam'
-    vcf_df = pd.read_csv('../data/common_SNPs/dbsnp_df.csv')
-    reads2remove, log_dict = list_supporting_reads(bamfile_path, vcf_df)
-    print(len(reads2remove))
     log_pd = pd.DataFrame.from_dict(log_dict)
     quick_check = sum(log_pd['supporting_reads']) == len(reads2remove)
     print(quick_check)
@@ -173,6 +129,19 @@ if __name__ == '__main__':
     log_pd['vaf'] = log_pd['supporting_reads'] / log_pd['total_reads']
     log_pd['normal af'] = log_pd['normal_reads'] / log_pd['total_reads']
     log_pd['noisy af'] = log_pd['alternative_reads'] / log_pd['total_reads']
+
+    samfile.close()
+
+    return reads2remove, log_pd
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    bamfile_path = '../data/healthy_chr22_merged-ready.bam'
+    vcf_df = pd.read_csv('../data/common_SNPs/dbsnp_df.csv')
+    reads2remove, log_pd = list_supporting_reads(bamfile_path, vcf_df)
     plt.figure(figsize=(10, 5))
     plt.title('SNV')
     sns.histplot(data=log_pd[log_pd['type'] == 'SNV'][['vaf', 'normal af', 'noisy af']], bins=100,  stat="probability")
