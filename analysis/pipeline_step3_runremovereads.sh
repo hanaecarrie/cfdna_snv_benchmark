d766710f1a293f0c9638d0ce6164de89787e6003
hanaecarrie <hanae.carrie@free.fr>
2021-03-22 18:30:00 +0800

    [add]: quick final test that the filtration worked

--- analysis/pipeline_step3_runremovereads.sh
+++ analysis/pipeline_step3_runremovereads.sh
@@ -12,3 +12,16 @@ fi
 /mnt/projects/simngl/wgs/tools/anaconda3/bin/java -jar /mnt/projects/carriehc/cfDNA/utils/picard.jar FilterSamReads -I $path_data/data/healthy_chr22_merged-ready.bam -O $path_data/data/pooled_healthy/healthy_chr22_merged-ready_filter_${patient}.bam --READ_LIST_FILE $path_data/data/prepare_pooled_healthy/readfile_${patient}_total.txt --FILTER excludeReadList
 
 /mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools index $path_data/data/pooled_healthy/healthy_chr22_merged-ready_filter_${patient}.bam
+
+# test the program worked
+echo "start testing..."
+read_id="$(shuf -n 1 $path_data/data/prepare_pooled_healthy/readfile_${patient}_total.txt)"
+export read_id
+echo "pick a random read to filter out in the readfile"
+echo $read_id
+echo "test read ID is present in original bam"
+samtools view $path_data/data/healthy_chr22_merged-ready.bam | grep $read_id
+echo "test read ID is not present in processed bam"
+samtools view $path_data/data/pooled_healthy/healthy_chr22_merged-ready_filter_${patient}.bam | grep $read_id
+echo "...done testing"
+
