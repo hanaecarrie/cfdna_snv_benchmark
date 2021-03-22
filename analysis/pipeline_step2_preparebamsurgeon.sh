5ca3937c5bba573ede3b8cd4381b0f0b8a923c77
hanaecarrie <hanae.carrie@free.fr>
2021-03-22 19:30:31 +0800

    [fix, update]: prepare bamsurgeon run snv

--- analysis/pipeline_step2_preparebamsurgeon.sh
+++ analysis/pipeline_step2_preparebamsurgeon.sh
@@ -10,3 +10,6 @@ export path_data=$2
 # STEP 2: preprare bamsurgeon input
 
 python3 ~/cfdna_snv_benchmark/analysis/preprocess_preparebamsurgeon.py $patientid_date $path_data
+
+split -l 10000 --numeric-suffixes $path_data/data/prepare_pooled_healthy/varfile_snv_${patient}_total.bed varfile_snv_${patient}_
+split -l 10000 --numeric-suffixes $path_data/data/prepare_pooled_healthy/varfile_indel_${patient}_total.bed varfile_indel_${patient}_
--- analysis/preprocess_runbamsurgeonsnv.sh
+++ analysis/preprocess_runbamsurgeonsnv.sh
@@ -12,17 +12,11 @@ echo $patient
 echo $chunk
 echo $ncpus
 
-if [ ! -d $path_data/CRC-${patient} ] ; then
-  mkdir $path_data/CRC-${patient}
-fi
-
-cd  $path_data/CRC-${patient} || exit
-
 python3 /mnt/projects/carriehc/cfDNA/utils/bamsurgeon/bin/addsnv.py \
-  -v $path_data/CRC-${patient}/varfile_snv_${patient}_dbsnp_${chunk}.bed \
-  -f $path_data/CRC-${patient}/healthy_chr22_merged-ready_filter.bam \
+  -v $path_data/data/prepare_pooled_healthy/varfile_snv_${patient}_${chunk}.bed \
+  -f $path_data/data/pooled_healthy/healthy_chr22_merged-ready_filter_${patient}.bam \
   -r /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa \
-  -o $path_data/CRC-${patient}-chunks/healthy_chr22_merged-ready_${patient}_filter_snv.bam \
+  -o $path_data/data/pooled_healthy/healthy_chr22_merged-ready_${patient}_filter_snv.bam \
   --mindepth 0 --maxdepth 200000 --ignoresnp --ignoreref --force --tagreads  \
   --picardjar /mnt/projects/carriehc/cfDNA/utils/picard.jar --aligner mem \
   --tmpdir $path_data/CRC-${patient}-chunks/addsnv.tmp --skipmerge -p $ncpus --seed 1
