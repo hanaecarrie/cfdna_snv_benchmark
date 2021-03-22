14d68d5137b76ba81ab468f7873fbb32766b03b1
hanaecarrie <hanae.carrie@free.fr>
2021-03-22 15:05:10 +0800

    [fix]: path issue

--- analysis/preprocess_preparebamsurgeon.py
+++ analysis/preprocess_preparebamsurgeon.py
@@ -18,7 +18,7 @@ patient_snps_df = pd.read_csv(args.path_data+'/data/patient_SNPs/patient_'+patie
 if not os.path.exists(args.path_data+'/data/prepare_pooled_healthy/log_'+patient_date.split('_')[0]+'_total.csv'):
     log_df = pd.read_csv(args.path_data+'/data/prepare_pooled_healthy/log_'+patient_date.split('_')[0]+'_dbsnp.csv')
     for log_path_genomad in [f for f in os.listdir(args.path_data+'/data/prepare_pooled_healthy/') if f.startswith('log_')]:
-        log_df_genomad = pd.read_csv(args.path_data+'/data/prepare_pooled_healthy/log_'+log_path_genomad)
+        log_df_genomad = pd.read_csv(args.path_data+'/data/prepare_pooled_healthy/'+log_path_genomad)
         log_df = pd.concat([log_df, log_df_genomad])
     log_df.to_csv(args.path_data+'/data/prepare_pooled_healthy/log_'+patient_date.split('_')[0]+'_total.csv', index=False)
 log_df = pd.read_csv(args.path_data+'/data/prepare_pooled_healthy/log_'+patient_date.split('_')[0]+'_total.csv')
