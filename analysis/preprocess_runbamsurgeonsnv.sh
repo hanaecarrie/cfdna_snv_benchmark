ced7bd22b808a0719e9337c346e61c1a498ca9d7
hanaecarrie <hanae.carrie@free.fr>
2021-03-22 18:29:25 +0800

    [update]: script path more general

--- analysis/preprocess_runbamsurgeonsnv.sh
+++ analysis/preprocess_runbamsurgeonsnv.sh
@@ -7,12 +7,24 @@ conda activate default
 export patient=$1
 export chunk=$2
 export ncpus=$3
+export path_data=$4
 echo $patient
 echo $chunk
 echo $ncpus
 
-cd /mnt/projects/carriehc/cfDNA/cfSNV/benchmark/pooled_healthy_pp/CRC-${patient}-chunks
+if [ ! -d $path_data/CRC-${patient} ] ; then
+  mkdir $path_data/CRC-${patient}
+fi
 
-python3 /mnt/projects/carriehc/cfDNA/utils/bamsurgeon/bin/addsnv.py -v /mnt/projects/carriehc/cfDNA/cfSNV/benchmark/pooled_healthy_pp/CRC-${patient}-chunks/varfile_snv_${patient}_dbsnp_${chunk}.bed -f /mnt/projects/carriehc/cfDNA/cfSNV/benchmark/pooled_healthy_pp/CRC-${patient}/healthy_chr22_merged-ready_filter.bam -r /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -o /mnt/projects/carriehc/cfDNA/cfSNV/benchmark/pooled_healthy_pp/CRC-${patient}-chunks/healthy_chr22_merged-ready_${patient}_filter_snv.bam --mindepth 0 --maxdepth 200000 --ignoresnp --ignoreref --force --tagreads  --picardjar /mnt/projects/carriehc/cfDNA/utils/picard.jar --aligner mem --tmpdir /mnt/projects/carriehc/cfDNA/cfSNV/benchmark/pooled_healthy_pp/CRC-${patient}-chunks/addsnv.tmp --skipmerge -p $ncpus --seed 1
+cd  $path_data/CRC-${patient} || exit
+
+python3 /mnt/projects/carriehc/cfDNA/utils/bamsurgeon/bin/addsnv.py \
+  -v $path_data/CRC-${patient}/varfile_snv_${patient}_dbsnp_${chunk}.bed \
+  -f $path_data/CRC-${patient}/healthy_chr22_merged-ready_filter.bam \
+  -r /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa \
+  -o $path_data/CRC-${patient}-chunks/healthy_chr22_merged-ready_${patient}_filter_snv.bam \
+  --mindepth 0 --maxdepth 200000 --ignoresnp --ignoreref --force --tagreads  \
+  --picardjar /mnt/projects/carriehc/cfDNA/utils/picard.jar --aligner mem \
+  --tmpdir $path_data/CRC-${patient}-chunks/addsnv.tmp --skipmerge -p $ncpus --seed 1
 
 
