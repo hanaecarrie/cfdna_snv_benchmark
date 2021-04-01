c99b9ee720cd563d84efcd988d9ca86eed1a28ea
hanaecarrie <hanae.carrie@free.fr>
2021-04-01 14:36:40 +0800

    [refrac]:pipeline pp pooled healthy with Ronin;

--- analysis/pipeline_step5_runbamsurgeonindel.sh
+++ analysis/pipeline_step5_runbamsurgeonindel.sh
@@ -12,15 +12,13 @@ echo $patient
 echo $chunk
 echo $ncpus
 
-cd ~/ || exit
+cd  ~/ || exit
 
-python3 ~/bamsurgeon/bin/addsnv.py \
-  -v $path_data/varfile_snv_${patient}_total.bed \
-  -f $path_data/healthy_chr22_merged-ready_${patient}_filter.bam \
+python3 ~/bamsurgeon/bin/addindel.py \
+  -v $path_data/varfile_indel_${patient}_total.bed \
+  -f $path_data/healthy_chr22_merged-ready_${patient}_filter_snv.bam \
   -r $path_data/GRCh37/GRCh37.fa \
-  -o $path_data/healthy_chr22_merged-ready_${patient}_filter_snv.bam \
+  -o $path_data/healthy_chr22_merged-ready_${patient}_filter_snv_indel.bam \
   --mindepth 0 --maxdepth 100000 --ignoresnp --ignoreref --force --tagreads  \
   --picardjar $path_data/picard.jar --aligner mem \
   --tmpdir addsnv_${patient}.tmp -p $ncpus --seed 1
-
-
