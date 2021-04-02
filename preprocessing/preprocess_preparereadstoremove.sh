6fd022c15b403700dfd4e3c4605a46c1646f9c7f
hanaecarrie <hanae.carrie@free.fr>
2021-04-02 14:11:28 +0800

    [fix]: missing argument

--- preprocessing/preprocess_preparereadstoremove.sh
+++ preprocessing/preprocess_preparereadstoremove.sh
@@ -4,14 +4,15 @@
 source /home/carriehc/miniconda2/etc/profile.d/conda.sh
 conda activate default
 
-export patientid_date=$1
-export snp_database=$2
-export chunk_start=$3
-export chunk_end=$4
-export path_data=$5
+export patient=$1
+export germline_vcf_name=$2
+export snp_database=$3
+export chunk_start=$4
+export chunk_end=$5
+export path_data=$6
 
 # STEP 1: preprare reads to remove
 
-python3 ~/cfdna_snv_benchmark/preprocessing/preprocess_preparereadstoremove.py $patientid_date $snp_database $chunk_start $chunk_end $path_data
+python3 ~/cfdna_snv_benchmark/preprocessing/preprocess_preparereadstoremove.py $patient $germline_vcf_name $snp_database $chunk_start $chunk_end $path_data
 
 
