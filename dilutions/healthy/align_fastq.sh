ed958715a1c0829e71f6394fe7f758bface5e7e6
hanaecarrie <hanae.carrie@free.fr>
2021-05-21 13:36:43 +0800

    [refrac]: clean folders, move data files to data folder

--- dilutions/healthy/align_fastq.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-cd  /mnt/projects/carriehc/cfDNA/cfSNV/benchmark/
-/mnt/projects/skanderupamj/wgs/bcbio_v107/bin/bwa mem -t 10 /mnt/projects/zhug/cfDNA/hg19-index/index /mnt/projects/skanderupamj/wgs/data/cfdna.crc/mix.samples/without.gatk.hap/qualfqs/healthy_chr22_merged_R1.fq.gz /mnt/projects/skanderupamj/wgs/data/cfdna.crc/mix.samples/without.gatk.hap/qualfqs/healthy_chr22_merged_R2.fq.gz  | /mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools sort -@ 10 -o  healthy_chr22_merged.bam -
-/mnt/projects/skanderupamj/wgs/bcbio_v107/bin/bamsormadup level=-1 threads=16 indexfilename=healthy_chr22_merged.bam.bai tmpfile=healthy_chr22_merged_tmp  < healthy_chr22_merged.bam   > healthy_chr22_merged_rc.bam
--- dilutions/healthy/preprare_healthy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
- /mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools view -b  /mnt/projects/carriehc/cfDNA/cfSNV/benchmark/healthy/healthy_chr22_merged-ready.bam 22 > /mnt/projects/carriehc/cfDNA/cfSNV/benchmark/dilutions_chr22/healthymerged/healthy_chr22_bis.bam 
