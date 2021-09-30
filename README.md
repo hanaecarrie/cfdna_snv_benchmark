fa027c3ce9c118c6b8dfade347825329b886c80b
hanaecarrie <hanae.carrie@free.fr>
2021-09-30 13:19:19 +0800

    [update]: readme and script to check pileups

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # cfdna_snv_benchmark
+
+
+
+## Create dilution series
+
+
+
+## Create spikein series
+
+
+
+## Prepare bcbio input
+
+
+
+## Run bcbio pipeline in Aquila
--- utils/check_pileup.sh
+++ utils/check_pileup.sh
@@ -4,61 +4,30 @@ source /mnt/projects/carriehc/cfDNA/anaconda3/etc/profile.d/conda.sh
 # def path and environment
 conda activate default
 
+cd /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/utils
 
-echo "#########################"
-echo "##### PATIENT 986 #######"
-echo "#########################"
+echo "######## PATIENT 986 #########"
+touch NCC_CRC-986_300316-CW-T_pileup.txt
+while IFS="" read -r p || [ -n "$p" ]
+do
+  printf '%s\n' "$p"
+  printf '%s\n' "$p" >> NCC_CRC-986_300316-CW-T_pileup.txt
+  chr="$( cut -f1 <<< "$p" )"; chr=${chr#"chr"} ; echo "$chr"
+  pos="$( cut -f2 <<< "$p" )"; echo "$pos"
+  name="$( cut -f3 <<< "$p" )"; echo "$name"
+  samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r $chr:$pos-$pos /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-986_300316-CW/bcbio_work/align/NCC_CRC-986_300316-CW-T/NCC_CRC-986_300316-CW-T-sort.bam >> NCC_CRC-986_300316-CW-T_pileup.txt
+done < /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/utils/Cancer226-targets_hg19.bed
 
-echo "######## EPHB2 ########"
-echo " T -> G "
-echo "chr1 pos 22895544 (hg38)"
-samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r 1:23222037-23222037 /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-986_300316-CW/bcbio_work/align/NCC_CRC-986_300316-CW-T/NCC_CRC-986_300316-CW-T-sort.bam
+#echo "######## PATIENT 1014 #########"
+#touch NCC_CRC-1014_090516-CW-T_pileup.txt
+#while IFS="" read -r p || [ -n "$p" ]
+#do
+#  printf '%s\n' "$p"
+#  printf '%s\n' "$p" >> NCC_CRC-1014_090516-CW-T_pileup.txt
+#  chr="$( cut -f1 <<< "$p" )"; chr=${chr#"chr"} ; echo "$chr"
+#  pos="$( cut -f2 <<< "$p" )"; echo "$pos"
+#  name="$( cut -f3 <<< "$p" )"; echo "$name"
+#  samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r $chr:$pos-$pos /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-1014_090516-CW/bcbio_work/align/NCC_CRC-1014_090516-CW-T/NCC_CRC-1014_090516-CW-T-sort.bam >> NCC_CRC-1014_090516-CW-T_pileup.txt
+#done < /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/utils/Cancer226-targets_hg19.bed
 
-echo "######## TP53 ########"
-echo " T -> C "
-echo "chr17 pos 7675076 (hg38)"
-samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r 17:7578394-7578394 /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-986_300316-CW/bcbio_work/align/NCC_CRC-986_300316-CW-T/NCC_CRC-986_300316-CW-T-sort.bam
 
-echo "######## SOX9 ########"
-echo " C -> CGA "
-echo "chr17 pos 72123617 to 72123619 (hg38)"
-samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r 17:70119758-70119760 /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-986_300316-CW/bcbio_work/align/NCC_CRC-986_300316-CW-T/NCC_CRC-986_300316-CW-T-sort.bam
-
-echo "######## PIK3CA ########"
-echo " A -> G "
-echo "chr3 pos 179218304 (hg38)"
-samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r 3:178936092-178936092 /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-986_300316-CW/bcbio_work/align/NCC_CRC-986_300316-CW-T/NCC_CRC-986_300316-CW-T-sort.bam
-
-echo "######## APC ########"
-echo " C -> T "
-echo "chr5 pos 112792446 (hg38)"
-samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r 5:112128143-112128143 /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-986_300316-CW/bcbio_work/align/NCC_CRC-986_300316-CW-T/NCC_CRC-986_300316-CW-T-sort.bam
-
-echo "#########################"
-echo "##### PATIENT 1014 ######"
-echo "#########################"
-
-echo "######## AKT1 ########"
-echo " C -> A "
-echo "chr14 pos 104773077 (hg38)"
-samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r 14:105239414-105239414 /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-1014_090516-CW/bcbio_work/align/NCC_CRC-1014_090516-CW-T/NCC_CRC-1014_090516-CW-T-sort.bam
-
-echo "######## TP53 ########"
-echo " CTG -> C "
-echo "chr17 pos 7675111 (hg38)"
-samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r 17:7578429-7578431 /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-1014_090516-CW/bcbio_work/align/NCC_CRC-1014_090516-CW-T/NCC_CRC-1014_090516-CW-T-sort.bam
-
-echo "######## SOX9 ########"
-echo " A -> T "
-echo "chr17 pos 72122793 (hg38)"
-samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r 17:70118934-70118934 /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-1014_090516-CW/bcbio_work/align/NCC_CRC-1014_090516-CW-T/NCC_CRC-1014_090516-CW-T-sort.bam
-
-echo "######## PIK3CA ########"
-echo " A -> G "
-echo "chr3 pos 179234297 (hg38)"
-samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r 3:178952085-178952085 /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-1014_090516-CW/bcbio_work/align/NCC_CRC-1014_090516-CW-T/NCC_CRC-1014_090516-CW-T-sort.bam
-
-echo "######## APC ########"
-echo " G -> T "
-echo "chr5 pos 112839783 (hg38)"
-samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r 5:112175480-112175480 /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-1014_090516-CW/bcbio_work/align/NCC_CRC-1014_090516-CW-T/NCC_CRC-1014_090516-CW-T-sort.bam
