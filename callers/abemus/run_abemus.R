2e29a2db4d80b92f1125514d2ef7523a68f0d385
hanaecarrie <hanae.carrie@free.fr>
2022-01-31 17:30:28 +0800

    [abemus]: first trial on Aquila

---
+++ callers/abemus/run_abemus.R
@@ -0,0 +1,56 @@
+library(abemus)
+library(data.table)
+library(parallel)
+
+outdir <- "/mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/callers/abemus/"
+sample.info.file <- "/mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/callers/abemus/infofile.tsv"
+targetbed <- "/mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/callers/abemus/regions_chr22_bis.bed"
+pacbamfolder <- "/mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/callers/abemus/pacbam_data/"
+pacbamfolder_bychrom <- "/mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/callers/abemus/pacbam_data_bychrom/"
+
+setwd(outdir)
+
+# split pacbam outputs by chrom
+
+split_pacbam_bychrom(targetbed=targetbed, pacbamfolder=pacbamfolder, pacbamfolder_bychrom=pacbamfolder_bychrom)
+targetbp_list <- bed2positions(targetbed=targetbed, get_only_chromosomes=TRUE)
+
+# compute per-base error model
+
+outpbem <- compute_pbem(sample.info.file=sample.info.file, targetbed=targetbed, outdir=outdir, pacbamfolder_bychrom=pacbamfolder_bychrom)
+
+# outs
+
+head(outpbem$pbem_tab)
+outpbem$bperr_summary
+outpbem$bgpbem
+outpbem$mean_pbem
+
+# compute coverage-based and not-coverage-based allelic fraction thresholds
+
+outafth <- compute_afthreshold(outdir=outdir, pbem_dir=file.path(outdir,"BaseErrorModel"))
+
+# outs
+
+head( outafth$th_results )
+head( outafth$th_results_bin )
+head( outafth$datacount_bin )
+
+# call snvs in case samples
+
+calls <- callsnvs(sample.info.file=sample.info.file, controls_dir=file.path(outdir,"Controls"), pbem_dir=file.path(outdir,"BaseErrorModel"), detection.specificity=0.995, replicas=10, outdir=outdir, outdir.calls.name="Results", targetbed=targetbed, pacbamfolder_bychrom=pacbamfolder_bychrom)
+head(calls$tabsnvs_index)
+
+tabindex <- calls$tabsnvs_index
+
+# compute mean coverage
+
+tabindex <- get_case_mean_coverage(tabindex = tabindex, pacbamfolder_bychrom = pacbamfolder_bychrom)
+target_size <- get_target_size(targetbed=targetbed, Mbp = TRUE)
+
+# apply pbem scaling factor to calls
+
+calls$tabsnvs_index_scalfact <- apply_scaling_factor(tabindex=tabindex, target_size=target_size, use.optimal.R=TRUE)
+
+head(calls$tabsnvs_index_scalfact) 
+
---
+++ callers/abemus/run_abemus.sh
@@ -0,0 +1,5 @@
+source /mnt/projects/carriehc/cfDNA/anaconda3/etc/profile.d/conda.sh
+conda activate cfSNV
+
+cd /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/callers/abemus
+Rscript run_abemus.R
