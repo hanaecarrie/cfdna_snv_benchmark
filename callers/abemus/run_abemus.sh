fc91e54a1ac77ef84a8078817fd810b177769057
hanaecarrie <hanae.carrie@free.fr>
2022-02-02 10:54:09 +0800

    [edit]: rm old abemus and ignore its intermediate files

--- callers/abemus/run_abemus.sh
+++
@@ -1,5 +0,0 @@
-source /mnt/projects/carriehc/cfDNA/anaconda3/etc/profile.d/conda.sh
-conda activate cfSNV
-
-cd /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/callers/abemus
-Rscript run_abemus.R
