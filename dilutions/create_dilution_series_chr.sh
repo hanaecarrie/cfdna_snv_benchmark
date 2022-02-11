7e298ac8ec596eab0a84ebc1cee50fade716dd4a
hanaecarrie <hanae.carrie@free.fr>
2022-02-11 18:45:25 +0800

    [fix]: create mixture series initial t and h bams

--- dilutions/create_dilution_series_chr.sh
+++ dilutions/create_dilution_series_chr.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# def path and environment
-source /mnt/projects/carriehc/cfDNA/anaconda3/etc/profile.d/conda.sh
-conda activate default
-
 # function to parse config file
 function parse_yaml {
    local prefix=$2
@@ -21,7 +17,6 @@ function parse_yaml {
       }
    }'
 }
-
 
 while getopts ":c:" opt; do
   case $opt in
@@ -47,6 +42,44 @@ echo $outputfolder
 echo $tffile
 
 if [ ! -d $outputfolder ] ; then mkdir $outputfolder ; fi
+
+export outputdir=$outputfolder/dilutions_${samplename_tumor}/dilution_chr${chr}_${samplename_tumor}_${dilutionfactor_tumor}_${samplename_healthy}_${dilutionfactor_healthy}
+if [ ! -d $outputfolder/dilutions_${samplename_tumor} ] ; then mkdir $outputfolder/dilutions_${samplename_tumor} ; fi
+echo $outputdir
+echo $tumordir
+echo $healthydir
+echo $buffycoatdir
+
+export sample_tumor_chr=$tumordir/${samplename_tumor}_chr${chr}.bam
+export sample_healthy_chr=$healthydir/${samplename_healthy}_chr${chr}.bam
+export sample_buffycoat_chr=$buffycoatdir/${samplename_buffycoat}_chr${chr}.bam
+echo $sample_tumor_chr
+echo $sample_healthy_chr
+
+export tumor_chr_coverage=$tumordir/coverage_${samplename_tumor}_chr${chr}.txt
+export healthy_chr_coverage=$healthydir/coverage_${samplename_healthy}_chr${chr}.txt
+echo $tumor_chr_coverage
+echo $healthy_chr_coverage
+
+cp $config_file $outputfolder/
+
+# Select chr only
+echo "Select chr ${chr} only for the tumor and the healthy sample..."
+if [ ! -d $tumordir ] ; then mkdir $tumordir ; fi
+
+if [ ! -f $sample_tumor_chr ] ; then /mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools view -b $sample_tumor $chr > $sample_tumor_chr ; fi
+if [ ! -f ${sample_tumor_chr}.bai ] ; then /mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools index $sample_tumor_chr ; fi
+if  [ ! -f $tumor_chr_coverage ] ; then export tumor_cov=$(/mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools depth -a $sample_tumor_chr | awk '{sum+=$3} END {print sum/NR}') ; echo $tumor_cov >  $tumor_chr_coverage ; else export tumor_cov=$(cat $tumor_chr_coverage) ; fi
+if [ ! -d $healthydir ] ; then mkdir $healthydir ; fi
+if [ ! -f $sample_healthy_chr ] ; then /mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools view -b $sample_healthy $chr > $sample_healthy_chr ; fi
+if [ ! -f ${sample_healthy_chr}.bai ] ; then /mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools index $sample_healthy_chr ; fi
+if  [ ! -f $healthy_chr_coverage ] ; then export healthy_cov=$(/mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools depth -a $sample_healthy_chr | awk '{sum+=$3} END {print sum/NR}') ; echo $healthy_cov >  $healthy_chr_coverage ; else export healthy_cov=$(cat $healthy_chr_coverage) ; fi
+
+# check buffy coat select chr exists
+echo "buffy coat..."
+if [ ! -d $buffycoatdir ] ; then mkdir $buffycoatdir ; fi
+if [ ! -f $sample_buffycoat_chr ] ; then /mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools view -b $sample_buffycoat $chr > $sample_buffycoat_chr ; fi
+if [ ! -f ${sample_buffycoat_chr}.bai ] ; then  /mnt/projects/skanderupamj/wgs/bcbio_v107/bin/samtools index $sample_buffycoat_chr ; fi
 
 for dilutionfactor in $dilutionfactors ;
 
