74d95bf1efaf0375d25975fd397ad6c4272746cb
hanaecarrie <hanae.carrie@free.fr>
2022-02-11 11:24:22 +0800

    [refrac]: ABEMUS working on Ronin

--- callers/ABEMUS/ABEMUS_Aquila/generate_infofile.sh
+++
@@ -1,7 +0,0 @@
-
-touch infofile_test.tsv
-
-for dil in /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/data/dilutions_series_chr22/dilutions_CRC-986_100215/dilution*_CRC-986_300316*/*.sorted.bam ;
-do echo -e '986_100215_chr22\t'$(basename $dil .bam)'\t'$dil'\tCRC-986_100215-BC_chr22\`t/mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/data/dilutions_series_chr22/NCC_CRC-986_100215-CW-N/CRC-986_100215-BC_chr22.bam'  >> infofile_test.tsv ;
-done
-
