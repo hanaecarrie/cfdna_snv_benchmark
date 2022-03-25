309e25f899727128dd4b8cef881cf23663bc7f63
hanaecarrie <hanae.carrie@free.fr>
2022-03-25 18:50:20 +0800

    [refrac]: clean abemus folder

--- callers/ABEMUS/run_abemus_all.sh
+++
@@ -1,7 +0,0 @@
-#bash run_abemus_i.sh 04 &
-#bash run_abemus_i.sh 05 &
-#bash run_abemus_i.sh 06 &
-bash run_abemus_i.sh 07 &
-bash run_abemus_i.sh 08 &
-bash run_abemus_i.sh 09 &
-#bash run_abemus_i.sh 10 &
--- callers/ABEMUS/run_pacbam.sh
+++
@@ -1,7 +0,0 @@
-
-#~/bin/pacbam/pacbam bam=../data/CRC-986_100215-BC_chr22.bam  bed=../chr22_data/exome_chr22_hg19.bed vcf=homo_sapiens-chr22_edited.vcf fasta=../data/GRCh37/GRCh37.fa strandbias mode=5 out=PaCBAM_outdir
-
-for file in ~/dilutions_chr22/*.bam ; 
-do echo $file ;
-	~/bin/pacbam/pacbam bam=$file  bed=../chr22_data/exome_chr22_hg19.bed vcf=homo_sapiens-chr22_edited.vcf fasta=../data/GRCh37/GRCh37.fa strandbias mode=5 out=PaCBAM_outdir
-done
