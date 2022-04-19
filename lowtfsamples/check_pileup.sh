a629159d94fd8a3cc2815052da5185a4b7807732
hanaecarrie <hanae.carrie@free.fr>
2022-04-19 15:09:55 +0800

    [feat]: pileup and vaf

--- lowtfsamples/check_pileup.sh
+++ lowtfsamples/check_pileup.sh
@@ -4,30 +4,25 @@ source /mnt/projects/carriehc/cfDNA/anaconda3/etc/profile.d/conda.sh
 # def path and environment
 conda activate default
 
-cd /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/utils
+cd /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/lowtfsamples/
 
-echo "######## PATIENT 986 #########"
-touch NCC_CRC-986_300316-CW-T_pileup.txt
+export bamfile=$1
+export bedfile=$2
+export outputfile=$3
+export refgenome=$4
+
+
+
+echo "######## ${outputfile} #########"
+touch $outputfile
 while IFS="" read -r p || [ -n "$p" ]
 do
   printf '%s\n' "$p"
-  printf '%s\n' "$p" >> NCC_CRC-986_300316-CW-T_pileup.txt
-  chr="$( cut -f1 <<< "$p" )"; chr=${chr#"chr"} ; echo "$chr"
-  pos="$( cut -f2 <<< "$p" )"; echo "$pos"
-  name="$( cut -f3 <<< "$p" )"; echo "$name"
-  samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r $chr:$pos-$pos /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-986_300316-CW/bcbio_work/align/NCC_CRC-986_300316-CW-T/NCC_CRC-986_300316-CW-T-sort.bam >> NCC_CRC-986_300316-CW-T_pileup.txt
-done < /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/utils/Cancer226-targets_hg19.bed
-
-#echo "######## PATIENT 1014 #########"
-#touch NCC_CRC-1014_090516-CW-T_pileup.txt
-#while IFS="" read -r p || [ -n "$p" ]
-#do
-#  printf '%s\n' "$p"
-#  printf '%s\n' "$p" >> NCC_CRC-1014_090516-CW-T_pileup.txt
-#  chr="$( cut -f1 <<< "$p" )"; chr=${chr#"chr"} ; echo "$chr"
-#  pos="$( cut -f2 <<< "$p" )"; echo "$pos"
-#  name="$( cut -f3 <<< "$p" )"; echo "$name"
-#  samtools mpileup -f /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa -r $chr:$pos-$pos /mnt/projects/skanderupamj/wgs/data/cfdna.crc/NCC_CRC-1014_090516-CW/bcbio_work/align/NCC_CRC-1014_090516-CW-T/NCC_CRC-1014_090516-CW-T-sort.bam >> NCC_CRC-1014_090516-CW-T_pileup.txt
-#done < /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/utils/Cancer226-targets_hg19.bed
-
+  printf '%s\n' "$p" >> $outputfile
+  chr="$( cut -f1 <<< "$p" )"; if [ $refgenome ==  /mnt/projects/carriehc/cfDNA/data/refgenome/GRCh37/GRCh37.fa ] ; then chr=${chr#"chr"} ; fi ; echo "$chr"
+  startpos="$( cut -f2 <<< "$p" )"; echo "$startpos"
+  endpos="$( cut -f3 <<< "$p" )"; echo "$endpos"
+  name="$( cut -f4 <<< "$p" )"; echo "$name"
+  samtools mpileup -f $refgenome -r $chr:$startpos-$endpos $bamfile >> $outputfile
+done < $bedfile
 
