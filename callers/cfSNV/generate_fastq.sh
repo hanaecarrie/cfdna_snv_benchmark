ddfbe9dd54a8cc03c770ed7db205e5a49be5560a
hanaecarrie <hanae.carrie@free.fr>
2022-05-10 19:26:18 +0800

    [feat]: generate fastq for cfSNV separately

---
+++ callers/cfSNV/generate_fastq.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source /home/ubuntu/anaconda3/etc/profile.d/conda.sh
+conda activate cfSNV
+
+
+export dilutionseriesfolder=$1
+export normal=$2
+export config_file=/home/ubuntu/cfdna_snv_benchmark/callers/cfSNV/config_generate_fastq.yaml
+
+for plasma in ${dilutionseriesfolder}/*/*[Tx].bam ; do
+
+echo $plasma
+
+export plasmafastqoutdir=$(dirname $plasma)
+export plasmafastqoutdir=$(echo "${plasmafastqoutdir/cfdnaseries/data}")  
+echo $plasmafastqoutdir
+if [ ! -d $plasmafastqoutdir ] ; then mkdir -p $plasmafastqoutdir ; fi
+if [ ! -f $plasmafastqoutdir/$(basename $plasma .bam)_R1.fastq.gz ] ; then
+	python /home/ubuntu/cfdna_snv_benchmark/callers/cfSNV/generate_fastqs_yaml.py  -i $plasma -t $plasmafastqoutdir/tmp -o $plasmafastqoutdir -c $config_file  & 
+fi
+
+done
+
+export normalfastqoutdir=$(dirname $normal)
+export normalfastqoutdir=$(echo "${normalfastqoutdir/cfdnaseries/data}") 
+echo $normalfastqoutdir
+if [ ! -d $normalfastqoutdir ] ; then mkdir -p $normalfastqoutdir ; fi
+if [ ! -f $normalfastqoutdir/$(basename $normal .bam)_R1.fastq.gz ] ; then
+	python /home/ubuntu/cfdna_snv_benchmark/callers/cfSNV/generate_fastqs_yaml.py -i $normal -t $normalfastqoutdir/tmp -o $normalfastqoutdir -c $config_file & 
+fi
---
+++ callers/cfSNV/run_generate_fastq.sh
@@ -0,0 +1,6 @@
+
+
+bash generate_fastq.sh /mnt/cfdnaseries/mixtures/mixtures_chr3/mixtures_chr3_CRC-1014_180816-CW-T_CRC-1014_090516-CW-T /mnt/cfdnaseries/initialsamples/initialsamples_chr3/initialsamples_chr3_CRC-1014-CW-N/CRC-1014-CW-N_chr3.bam
+#bash generate_fastq.sh /mnt/cfdnaseries/mixtures/mixtures_chr3/mixtures_chr3_CRC-986_100215-CW-T_CRC-986_300316-CW-T /mnt/cfdnaseries/initialsamples/initialsamples_chr3/initialsamples_chr3_CRC-986-CW-N/CRC-986-CW-N_chr3.bam
+#bash generate_fastq.sh /mnt/cfdnaseries/mixtures/mixtures_chr3/mixtures_chr3_CRC-123_310715-CW-T_CRC-123_121115-CW-T /mnt/cfdnaseries/initialsamples/initialsamples_chr3/initialsamples_chr3_CRC-123-CW-N/CRC-123-CW-N_chr3.bam
+
