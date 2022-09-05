7f090ed1ec09b19d34bd660cc5268ae928517d52
hanaecarrie <hanae.carrie@free.fr>
2022-09-05 17:59:23 +0800

    [update]: mixtures exome all chroms

--- mixtures/qsub_create_mixtures.sh
+++ mixtures/qsub_create_mixtures.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 #SBATCH -p normal
-#SBATCH -J chr1_986_mixtures_wes
+#SBATCH -J chrall_986_mixtures_wes
 #SBATCH -t 24:00:00
 #SBATCH -N 1
 #SBATCH --mem 24000
-#SBATCH --output=/rfs-storageservice/GIS/Projects/LOCCG/carriehc/data_wes/logs/z.chr1_986_mixture_wes.o
-#SBATCH --error=/rfs-storageservice/GIS/Projects/LOCCG/carriehc/data_wes/logs/z.chr1_986_mixtures_wes.e
+#SBATCH --output=/rfs-storageservice/GIS/Projects/LOCCG/carriehc/data_wes/logs/z.chrall_986_mixture_wes.o
+#SBATCH --error=/rfs-storageservice/GIS/Projects/LOCCG/carriehc/data_wes/logs/z.chrall_986_mixtures_wes.e
 
 source /apps/anaconda3-individual-edition/2020.11/etc/profile.d/conda.sh
 
 cd /home/users/astar/gis/carriehc/cfdna_snv_benchmark/mixtures
 
-export chr=1
-
-bash create_mixtures_series_chr.sh -c /home/users/astar/gis/carriehc/cfdna_snv_benchmark/mixtures/config/config_mixtures_chr${chr}_CRC-986_100215-CW-T_CRC-986_300316-CW-T_WES.yml
+bash create_mixtures_series_chr.sh -c /home/users/astar/gis/carriehc/cfdna_snv_benchmark/mixtures/config/config_mixtures_chrall_CRC-986_100215-CW-T_CRC-986_300316-CW-T_WES.yml
 
 
