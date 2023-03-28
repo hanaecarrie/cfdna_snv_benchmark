533a2f60ea2115df48fd20481f703629d3373fe9
hanaecarrie <hanae.carrie@free.fr>
2023-03-28 15:25:52 +0800

    [update] save before pull

--- callers/copy_calls_to_local.sh
+++ callers/copy_calls_to_local.sh
@@ -1,11 +1,6 @@
 
-export datapath=$1 #'/Users/hanae/Repositories/cfdna_snv_benchmark/'
-export seriespath=$2 #'data/mixtures/mixtures_chr22/mixtures_chr22_CRC-1014_180816-CW-T/'
-
-# cfdna callers on Ronin
-for sample in $datapath/$seriespath/*[Tx] ; 
-do export sample=$(basename $sample)
-echo $sample
-echo /mnt/projects/skanderupamj/wgs/bcbio_v107/bin/aws s3 cp s3://cfdna-mutation-calling.store.genome.sg/${seriespath}${sample}/calls $datapath/$seriespath/$sample/calls --profile hanae --recursive
-/mnt/projects/skanderupamj/wgs/bcbio_v107/bin/aws s3 cp s3://cfdna-mutation-calling.store.genome.sg/${seriespath}${sample}/calls $datapath/$seriespath/$sample/calls --profile hanae --recursive
-done
+export dilutionseries=$1
+echo aws s3 ls s3://cfdna-mutation-calling.store.genome.sg/$dilutionseries/ --profile hanae
+aws s3 ls s3://cfdna-mutation-calling.store.genome.sg/$dilutionseries/ --profile hanae
+echo aws s3 cp s3://cfdna-mutation-calling.store.genome.sg/$dilutionseries /Users/hanae/Repositories/cfdna_snv_benchmark/$dilutionseries --profile hanae --recursive --exclude '*.bam' 
+aws s3 cp s3://cfdna-mutation-calling.store.genome.sg/$dilutionseries /Users/hanae/Repositories/cfdna_snv_benchmark/$dilutionseries --profile hanae --recursive --exclude '*.bam' 
