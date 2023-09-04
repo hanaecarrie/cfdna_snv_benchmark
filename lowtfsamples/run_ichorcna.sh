dabd25e59c0722f92c583d3824b8a0c25341fbfa
hanaecarrie <hanae.carrie@free.fr>
2023-09-04 15:25:15 +0800

    [update]: path pluto ichorcna

--- lowtfsamples/run_ichorcna.sh
+++ lowtfsamples/run_ichorcna.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-source /mnt/projects/carriehc/cfDNA/anaconda3/etc/profile.d/conda.sh
+#source /mnt/projects/carriehc/cfDNA/anaconda3/etc/profile.d/conda.sh
+source /apps/anaconda3-distribution/2022.10/etc/profile.d/conda.sh
 # def path and environment
-conda activate ichorcna
+conda activate ichorCNA
 
 export bamfile=$1
 export outputdir=$(dirname $bamfile)
 export saveid=$(basename $bamfile .bam)
-export ichorcna_extdata=$3
+export ichorcna_extdata=$2
 echo $bamfile
 echo $saveid
 echo $outputdir
-echo $ichorcna_extdata  # /mnt/projects/carriehc/cfDNA/anaconda3/envs/ichorcna/lib/R/library/ichorCNA/extdata/
+echo $ichorcna_extdata  # /home/users/astar/gis/carriehc/.conda/envs/ichorCNA/lib/R/library/ichorCNA/extdata
 
 if [ ! -f "$outputdir/${saveid}.wig" ] ; then
 echo "Create WIG file $outputdir/${saveid}.wig"
 # Create WIG Files
-/mnt/projects/carriehc/cfDNA/utils/hmmcopy_utils/bin/readCounter --window 1000000 --quality 20 --chromosome "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y" $bamfile > $outputdir/${saveid}.wig
+/home/users/astar/gis/carriehc/.conda/envs/ichorCNA/bin/readCounter --window 1000000 --quality 20 --chromosome "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y" $bamfile > $outputdir/${saveid}.wig
 echo "Done WIG file"
 fi
 
 # ichorCNA run
 echo "ichorCNA processing $outputdir/${saveid}.wig"
 if [ ! -f "$outputdir/${saveid}.cna.seg" ] ; then
-/mnt/projects/carriehc/cfDNA/anaconda3/envs/ichorcna/bin/Rscript /mnt/projects/carriehc/cfDNA/cfdna_snv/cfdna_snv_benchmark/utils/runIchorCNA.R \
+/home/users/astar/gis/carriehc/.conda/envs/ichorCNA/bin/Rscript /home/users/astar/gis/carriehc/cfdna_snv_benchmark/utils/runIchorCNA.R \
 --id $saveid \
 --WIG $outputdir/${saveid}.wig --ploidy "c(2,3)" --normal "c(0.5,0.6,0.7,0.8,0.9)" --maxCN 5 \
 --gcWig $ichorcna_extdata/gc_hg19_1000kb.wig \
