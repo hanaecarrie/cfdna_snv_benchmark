f3016969f0640b6089c217b9e7e44bab815f7df1
hanaecarrie <hanae.carrie@free.fr>
2023-09-04 15:23:34 +0800

    [update]: config script

--- callers/sinvict/create_config_yaml.sh
+++ callers/sinvict/create_config_yaml.sh
@@ -2,8 +2,25 @@
 
 cd config
 
+for i in {2..22}
+do
+        export oldconfigfile="config_sinvict_mixtures_chr1_BRA-412_240820-CW-T_BRA-412_060220-CW-T.yml"
+        export configfile=$(echo "${oldconfigfile/chr1/"chr${i}"}")
+        echo $configfile
+        cp $oldconfigfile $configfile
+        export search="chr1"
+        export replace="chr${i}"
+        sed -i "s/$search/$replace/g" $configfile
+        export search="_chr1_"
+        export replace="_chr${i}_"
+        sed -i "s/$search/$replace/g" $configfile
+        export search="chr: 1"
+        export replace="chr: ${i}"
+        sed -i "s/$search/$replace/g" $configfile
+done
 
 
+"""
 for i in {2..22}
 do
         export oldconfigfile="config_sinvict_mixtures_chr1_CRC-986_100215-CW-T_CRC-986_300316-CW-T_WGS.yml"
@@ -21,7 +38,6 @@ do
         sed -i "s/$search/$replace/g" $configfile
 done
 
-"""
 for i in {2..22}
 do
 	export oldconfigfile="config_sinvict_mixtures_chr1_CRC-1014_180816-CW-T_CRC-1014_090516-CW-T.yml"
--- callers/varnet/create_config_yaml.sh
+++ callers/varnet/create_config_yaml.sh
@@ -4,7 +4,7 @@ cd config
 
 for i in {2..22}
 do
-        export oldconfigfile="config_varnet_mixtures_chr1_CRC-123_310715-CW-T_CRC-123_121115-CW-T.yml"
+        export oldconfigfile="config_varnet_mixtures_chr1_BRA-412_240820-CW-T_BRA-412_060220-CW-T.yml"
         export configfile=$(echo "${oldconfigfile/chr1/"chr${i}"}")
         echo $configfile
         cp $oldconfigfile $configfile
@@ -21,6 +21,23 @@ done
 
 
 """
+for i in {2..22}
+do
+        export oldconfigfile="config_varnet_mixtures_chr1_CRC-123_310715-CW-T_CRC-123_121115-CW-T.yml"
+        export configfile=$(echo "${oldconfigfile/chr1/"chr${i}"}")
+        echo $configfile
+        cp $oldconfigfile $configfile
+        export search="chr1"
+        export replace="chr${i}"
+        sed -i "s/$search/$replace/g" $configfile
+        export search="_chr1_"
+        export replace="_chr${i}_"
+        sed -i "s/$search/$replace/g" $configfile
+        export search="chr: 1"
+        export replace="chr: ${i}"
+        sed -i "s/$search/$replace/g" $configfile
+done
+
 for i in {2..22}
 do
 	export oldconfigfile="config_varnet_mixtures_chr1_CRC-986_100215-CW-T_CRC-986_300316-CW-T.yml"
